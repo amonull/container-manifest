4d0b2b0deab9bc15818aff575e81956582057acd
amonull <90932550+amonull@users.noreply.github.com>
2025-02-26 22:28:19 +0000

    adding usable version of script
    script itself is very unrefined but usable, logger is removed was too much of a hassle to get it working

--- src/manifest.sh
+++ src/manifest.sh
@@ -1,126 +1,81 @@
 #!/bin/bash
 
-##########################################
-################ GLOBALS #################
-##########################################
-
-# TODO: read cli args and set these values like that
-__MANIFEST_FILE=""
-__LOG_FILE="$HOME/.cache/container-builder-automation-tool/logs.log"
-__IMAGE_BUILD_DIR="$(mktemp -d)"
-__CONTAINER_SCRIPTS_TMP_DIR="$(mktemp -d)"
-__CONTAINER_NAME="$(getName)"
-__CONTAINER_HOME="$(getHome)"
-__LOG_LEVEL="6"
-
-##########################################
-################ HELPERS #################
-##########################################
-
-##########################################
-############### CONTAINER ################
-##########################################
-
-writeScriptsPreToTmp() {
-    __writeScriptToTmp "pre" getScriptsPreLength getScriptsPreIndexed
-}
-
-writeScriptsPeriToTmp() {
-    __writeScriptToTmp "peri" getScriptsPeriLength getScriptsPeriIndexed
-}
-
-writeScriptsPostToTmp() {
-    __writeScriptToTmp "post" getScriptsPostLength getScriptsPostIndexed
-}
-
-__writeScriptToTmp() {
-    local scriptStage=$1
-    local stageGetListLengthCmd=$2
-    local stageGetFileContentsCmd=$3
-
-    for ((index=0; index <= $(stageGetListLengthCmd); index++)); do
-        local fileName="$index.sh"
-        local fileContent="$(stageGetFileContentsCmd $index)"
-
-        trace "writing $scriptStage script $fileName to $__CONTAINER_SCRIPTS_TMP_DIR"
-
-        install -m 777 <(echo $fileContent) "$__CONTAINER_SCRIPTS_TMP_DIR/$scriptStage/$fileName"
-    done
-}
 
 ##########################################
 ################## YAML ##################
 ##########################################
 
-getName() {
+yaml_getName() {
     __runYamlFilter '.container.name'
 }
 
-getHome() {
+yaml_getHome() {
     __runYamlFilter '.container.home'
 }
 
-getExportsLength() {
+yaml_getExportsLength() {
     __listGetLength '.container.exports'
 }
 
-getImportsLength() {
+yaml_getImportsLength() {
     __listGetLength '.container.imports'
 }
 
-getScriptsPreLength() {
+yaml_getScriptsPreLength() {
     __listGetLength '.container.scripts.pre'
 }
 
-getScriptsPeriLength() {
+yaml_getScriptsPeriLength() {
     __listGetLength '.container.scripts.peri'
 }
 
-getScriptsPostLength() {
+yaml_getScriptsPostLength() {
     __listGetLength '.container.scripts.post'
 }
 
-getExportsIndexed() {
+yaml_getExportsIndexed() {
     local index=$1
     __runYamlFilter ".container.exports[$index]"
 }
 
-getImportsIndexed() {
+yaml_getImportsIndexed() {
     local index=$1
     __runYamlFilter ".container.imports[$index]"
 }
 
-getScriptsPreIndexed() {
+yaml_getScriptsPreIndexed() {
     local index=$1
     __runYamlFilter ".container.scripts.pre[$index]"
 }
 
-getScriptsPeriIndexed() {
+yaml_getScriptsPeriIndexed() {
     local index=$1
     __runYamlFilter ".container.scripts.peri[$index]"
 }
 
-getScriptsPostIndexed() {
+yaml_getScriptsPostIndexed() {
     local index=$1
     __runYamlFilter ".container.scripts.post[$index]"
 }
 
-getContainerFile() {
+yaml_getContainerFile() {
     __runYamlFilter '.image.Containerfile'
 }
 
-getImageFilesLength() {
+yaml_getImageFilesLength() {
     __listGetLength '.image.files'
 }
 
-getImageFilesIndexedName() {
+yaml_getImageFilesIndexedName() {
     local index=$1
-    __runYamlFilter ".image.files[$index] | keys"
+    # yq returns "- fileName" cut used to remove "- "
+    cut -c3- <<< "$(__runYamlFilter ".image.files[$index] | keys")"
 }
 
-getImageFilesIndexed() {
+yaml_getImageFilesIndexed() {
     local index=$1
-    __runYamlFilter ".image.files[$index]"
+    local fileName=$2
+    __runYamlFilter ".image.files[$index].\"$fileName\""
 }
 
 __listGetLength() {
@@ -129,8 +84,6 @@ __listGetLength() {
     local result=$(__runYamlFilter "$filter | length")
 
     if [[ -z "$result" ]]; then
-        trace "$filter is an empty list"
-
         echo -n '0'
     else
         echo -n $result
@@ -139,27 +92,22 @@ __listGetLength() {
 
 __runYamlFilter() {
     local filter=$1
-
-    local result="$(yq $filter $__MANIFEST_FILE)"
-
-    echo $filter
-
-    debug "Ran $filter on $__MANIFEST_FILE"
+    local result="$(yq eval "$filter" "$__MANIFEST_FILE" || echo "")"
 
     if [[ "$result" == "null" ]]; then
         result=""
-        warn "$filter returned null"
     fi
 
     echo -n "$result"
 }
 
+
 ##########################################
 ############### DISTROBOX ################
 ##########################################
 
-# NOTE: runs a single command (preferebly a full path to the cmd), opts cannot be supplied
-distrobox-run() {
+# NOTE: runs a single command (preferably a full path to the cmd), opts cannot be supplied
+distrobox_run() {
     # TODO: find a way to force bash to make a distinction between $2 and $3 inside $@
     # currently when 'bash' '-c' 'echo hello world' is pushed inside of $@ it becomes a
     # single string instead of a list of strings
@@ -167,43 +115,37 @@ distrobox-run() {
 }
 
 # NOTE: this will run a script only in bash, bash is supplied the flag '-c'
-distrobox-run-cmd() {
+distrobox_run-cmd() {
     /usr/bin/distrobox-enter --name $__CONTAINER_NAME -- '/usr/bin/bash' '-c' "$@"
 }
 
-distrobox-export() {
+distrobox_export() {
     local pathToBinOrApp=$1
     local isBinaryOrApp=""
 
     if [[ "$pathToBinOrApp" == "*.desktop" ]]; then
-        debug "app detected in export $pathToBinOrApp"
-
         isBinaryOrApp="-a"
 
         [[ -f $pathToBinOrApp ]] || pathToBinOrApp="$__CONTAINER_HOME/.local/share/applications/$pathToBinOrApp"
     else
-        debug "binary detected in export $pathToBinOrApp"
-
         isBinaryOrApp="-b"
 
         # assumes just name is given
         [[ -x $pathToBinOrApp ]] || pathToBinOrApp="$(which $pathToBinOrApp)"
     fi
 
-    [[ -f "$pathToBinOrApp" ]] || fatal 1 "could not find bin or app to export: $pathToBinOrApp"
+    [[ -f "$pathToBinOrApp" ]] || exit 1
 
-    [[ "$isBinaryOrApp" == "-b" && -x "$pathToBinOrApp" ]] || fatal 1 "$pathToBinOrApp is not an executable"
+    [[ "$isBinaryOrApp" == "-b" && -x "$pathToBinOrApp" ]] || exit 1
 
-    distrobox-run-cmd "distrobox-export $isBinaryOrApp $pathToBinOrApp"
+    distrobox_run-cmd "distrobox-export $isBinaryOrApp $pathToBinOrApp"
 }
 
 # NOTE: this only works with binary apps or apps that are meant to be moved into ~/.local/bin/
 # and ideally a full path should be supplied
 # NOTE: this function does not run inside the container so container_home is used
-distrobox-import() {
+distrobox_import() {
     local import=$1
-
-    debug "importing $import to"
 
     mkdir -p $__CONTAINER_HOME/.local/bin
 
@@ -214,98 +156,137 @@ distrobox-host-exec $1 # WARNING: this will not work as it is in cat <<< EOF str
 EOF
 }
 
+distrobox_create_pod() {
+    local flags="--image localhost/$__CONTAINER_NAME --name $__CONTAINER_NAME"
+
+    if [[ -n "$__CONTAINER_HOME" ]]; then
+        flags="$flags --home $__CONTAINER_HOME"
+    fi
+
+    distrobox create $flags
+}
+
+distrobox_stop_container() {
+    distrobox stop --yes "$__CONTAINER_NAME"
+}
+
+
 ##########################################
 ################# PODMAN #################
 ##########################################
 
-buildImage() {
+podman_buildImage() {
     podman build --tag $__CONTAINER_NAME $__IMAGE_BUILD_DIR
 }
 
-processContainerfile() {
-    sed -i "s|{{\(.*\)}}|$__IMAGE_BUILD_DIR/\1|g" $__IMAGE_BUILD_DIR/Containerfile
+podman_writeContainerFileToTmp() {
+    local containerFile="$(yaml_getContainerFile)"
+    echo -e "$containerFile" > $__IMAGE_BUILD_DIR/Containerfile
 }
 
-writeFilesToTmp() {
-    for ((index=0; index <= $(getImageFilesLength); index++)); do
-        local fileName="$(getImageFilesIndexedName $index)"
-        local fileContent="$(getImageFilesIndexed $index)"
-        local fileParentDir="$(dirname $filename)"
-
-        trace "writing image file $fileName to $__IMAGE_BUILD_DIR"
+podman_writeFilesToTmp() {
+    for ((index=0; index != $(yaml_getImageFilesLength); index++)); do
+        local fileName="$(yaml_getImageFilesIndexedName "$index")"
+        local fileContent="$(yaml_getImageFilesIndexed "$index" "$fileName")"
+        local fileParentDir="$(dirname "$filename")"
 
         if [[ -d "$fileParentDir" ]]; then
             mkdir -p "$fileParentDir"
         fi
 
-        echo $fileContent > $__IMAGE_BUILD_DIR/$fileName
+        echo -e "$fileContent" > "$__IMAGE_BUILD_DIR/$fileName"
     done
 }
 
+
 ##########################################
-################ LOGGER ##################
+############### CONTAINER ################
 ##########################################
 
-fatal() {
-    local errCode=$1
-    local msg=$2
-    __writeMessage 0 $msg
-    exit $errCode
+container_writeScriptsPreToTmp() {
+    __writeScriptToTmp "pre" yaml_getScriptsPreLength yaml_getScriptsPreIndexed
 }
 
-err() {
-    local msg=$1
-    __writeMessage 1 $msg
+container_writeScriptsPeriToTmp() {
+    __writeScriptToTmp "peri" yaml_getScriptsPeriLength yaml_getScriptsPeriIndexed
 }
 
-warn() {
-    local msg=$1
-    __writeMessage 2 $msg
+container_writeScriptsPostToTmp() {
+    __writeScriptToTmp "post" yaml_getScriptsPostLength yaml_getScriptsPostIndexed
 }
 
-info() {
-    local msg=$1
-    __writeMessage 3 $msg
+__writeScriptToTmp() {
+    local scriptStage=$1
+    local stageGetListLengthCmd=$2
+    local stageGetFileContentsCmd=$3
+
+    for ((index=0; index != $($stageGetListLengthCmd); index++)); do
+        local fileName="$index.sh"
+        local fileContent="$($stageGetFileContentsCmd $index)"
+
+        install -Dm 777 <(echo -e "$fileContent") "$__CONTAINER_SCRIPTS_TMP_DIR/$scriptStage/$fileName"
+    done
 }
 
-debug() {
-    local msg=$1
-    __writeMessage 4 $msg
-}
 
-trace() {
-    local msg=$1
-    __writeMessage 5 $msg
-}
+##########################################
+################## MAIN ##################
+##########################################
+__MANIFEST_FILE="$HOME/Documents/tmp/manifest.yaml"
+__IMAGE_BUILD_DIR="$(mktemp -d)"
+__CONTAINER_SCRIPTS_TMP_DIR="$(mktemp -d)"
+__CONTAINER_NAME="$(yaml_getName)"
+__CONTAINER_HOME="$(yaml_getHome)"
 
-__writeMessage() {
-    local level=$1
-    local msg=$2
-    local date="$(date +"%D %T")"
+# [ write all files ]
+podman_writeContainerFileToTmp
+podman_writeFilesToTmp
+container_writeScriptsPreToTmp
+container_writeScriptsPeriToTmp
+container_writeScriptsPostToTmp
 
-    if [[ "$level" -gt "$__LOG_LEVEL" ]]; then
-        return
+# [ build image ]
+podman_buildImage
+
+[ exec pre scripts ]
+for preScript in $__CONTAINER_SCRIPTS_TMP_DIR/pre/*; do
+    if [[ -x "$preScript" ]]; then
+        bash "$preScript" "$__CONTAINER_NAME" "$__CONTAINER_HOME"
     fi
+done
 
-    echo "$date - [$(__logLevelToString $level)]: $msg" | tee -a $__LOG_FILE
-}
+# [ create container ]
+distrobox_create_pod
 
-__logLevelToString() {
-    local level=$1
+# [ exec peri scripts ]
+for script in $(distrobox_run-cmd "find /run/host/$__CONTAINER_SCRIPTS_TMP_DIR/peri/ -type f -executable -exec realpath {} \;"); do
+    distrobox_run-cmd "$script"
+done
 
-    case $level in
-        0) echo -n "FATAL" ;;
+# [ shut container down ]
+distrobox_stop_container
 
-        1) echo -n "ERR"   ;;
+# [ exec post scripts ]
+for script in $(distrobox_run-cmd "find /run/host/$__CONTAINER_SCRIPTS_TMP_DIR/post/ -type f -executable -exec realpath {} \;"); do
+    distrobox_run-cmd "$script"
+done
 
-        2) echo -n "WARN"  ;;
+# [ shut container down ]
+distrobox_stop_container
 
-        3) echo -n "INFO"  ;;
+# [ export apps ]
+for ((index=0; index != yaml_getExportsLength; index++)); do
+    local file="$(yaml_getExportsIndexed $index)"
 
-        4) echo -n "DEBUG" ;;
+    distrobox_export $file
+done
 
-        5) echo -n "TRACE" ;;
+# [ import apps ]
+for ((index=0; index != yaml_getExportsLength; index++)); do
+    local file="$(yaml_getExportsIndexed $index)"
 
-        *) echo -n "UNKOWN";;
-    esac
-}
+    distrobox_import $file
+done
+
+# [ shut container down ]
+distrobox_stop_container
